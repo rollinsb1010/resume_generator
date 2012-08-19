ec4c8f9b97d72e915215f4b6f2a6a3a6c46bab0e
Systho <systho@gmail.com>
2012-08-19 18:15:10 +0200

    allow destruction of project from nested attributes

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,6 +1,6 @@
 class Resume < ActiveRecord::Base
   has_many :projects, :dependent => :destroy
-  accepts_nested_attributes_for :projects
+  accepts_nested_attributes_for :projects, :allow_destroy => true
 
   validates :first_name, :presence => true
   validates :last_name, :presence => true
